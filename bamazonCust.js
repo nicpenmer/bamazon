3b5eebc8508ac49347aa8df90d8bf63427d93fe9
nicpenmer <nicole.penmer@gmail.com>
2018-04-21 08:10:22 -0400

    update

--- bamazonCust.js
+++ bamazonCust.js
@@ -24,43 +24,95 @@ connection.connect(function (err) {
     start();
 });
 
-//make a variable to hold customer order
-var customerOrder =[]; 
+
 
 //start function that prompts that welcomes the user to bamazon and prompts them to see the items for sale
 function start() {
     console.log("Welcome to BAMAZON.");
     console.log("Below you will find all of our offerings for sale ")
-        // once you have the items, prompt the user for which they'd like to buy
-        inquirer.prompt([
-            {
-              name: "Items for sale",
-              type: "list",
-              message: "Which Item would you like to buy?",
-              choices: function readProducts() {
-                console.log("Selecting all of Products...\n");
-                connection.query("SELECT id, Name, Department, Price FROM Products", function (err, res) {
-                    if (err) throw err;
-                    // Log all results of the SELECT statement
-                    console.log(res);
-                   
-                });
-            }
-            },
-          ]).then(function(answer) {
-            var query = "SELECT id, Name, Price FROM Products WHERE ?";
-            connection.query(query, { id: answer.id }, function(err, res) {
-              for (var i = 0; i < res.length; i++) {
-                console.log("id: " + res[i].id + " || Name: " + res[i].Name + " || Price: $" + res[i].Price);
-              }
-              sale();
-            });
-          });
-
+    // once you have the items, prompt the user for which they'd like to buy
+    var query = "SELECT id, Name, Price FROM Products WHERE ?";
+    //connection to data base to display id, name and price of items for sale
+    connection.query(query, { id: answer.id }, function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log("id: " + res[i].id + " || Name: " + res[i].Name + " || Price: $" + res[i].Price);
+        }
+        getId();
+    });
 };
 
+//function to promput user for ID using inquirer 
+function getId() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        inquirer
+            .prompt([
+                {
+                    name: "id",
+                    type: "input",
+                    message: "Select the item's ID Number to Purchase.",
+                    validate: function (value) {
+                        if (isNaN(value) === false) {
+                            return true;
+                        }
+                        return false;
+                    }
+                },
+                {
+                    name: "quantity",
+                    type: "input",
+                    message: "How many would you like to purchase?",
+                    validate: function (value) {
+                        if (isNaN(value) === false) {
+                            return true;
+                        }
+                        return false;
+                    }
+                }
+            ])
+            .then(function answer(err, res) {
+                //print response
+                console.log("You would like to purchase" + res[i].Quantity + " of item number :" + res[i].id + "at $" + res[i].Price + "your total is $" + (res[i].Quantity * res[i].Price));
+                connection.query(
+                    "UPDATE Products SET ? WHERE ?",
+                    [
+                        {
+                            id: answer.id
+                        }
+                    ],
+                    function (err, res) {
+                        if (err) throw err;
+                        if (results.length > 0) {
+                            if (results[0].Quantity < product.Quantity) {
+                                console.log("Sorry but we are low on that product");
+                                console.log("Please Select another item to purchase.")
+                                start();
+                            } else {
+                                console.log("Your Purchase is ready.");
+                                console.log("Thank you. Have a nice one.");
+                                start();
+                            }
+                        }
+                    }
+                );
+                console.log("Sorry, we are unable to fill that request at this time. perhaps next time.");
+                start();
 
+            });
+    });
+};
+//update quantity in databadse
+function updateProducts() {
+    connection.query(
+        'UPDATE Products SET Quantity = Quantity - ?, ',
+        function (err) {
+            if (err) throw err;
+            start();
+        }
+    );
+};
 
-function sale(){
-    console.log("You would like to purchase item number :" + res[i].id + "at" + res[i].Price + "your total is" + res[i].Price);
-}
+//call the function to update the DB
+updateProducts();
+
